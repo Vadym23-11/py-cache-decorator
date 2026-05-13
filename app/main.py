5f64db01f8c3d8b3fac9d151671000b0d733ca96
vadym <vadym.rurka01@gmail.com>
2026-05-13 17:12:05 +0200

    New new new new  solution

--- app/main.py
+++ app/main.py
@@ -6,13 +6,15 @@ def cache(func: Callable) -> Callable:
 
     def wrapper(*args, **kwargs) -> Any:
         key = (args, frozenset(kwargs.items()))
+
         if key not in cache_dict:
             print("Calculating new result")
             cache_dict[key] = func(*args, **kwargs)
-            return cache_dict[key]
         else:
             print("Getting from cache")
-            return cache_dict[key]
+
+        # Повертаємо результат в одному місці для обох випадків
+        return cache_dict[key]
 
     return wrapper
 
@@ -25,11 +27,3 @@ def long_time_func(num_a: int, num_b: int, num_c: int) -> int:
 @cache
 def long_time_func_2(n_tuple: tuple, power: int) -> list:
     return [number ** power for number in n_tuple]
-
-
-long_time_func(1, 2, 3)
-long_time_func(2, 2, 3)
-long_time_func_2((5, 6, 7), 5)
-long_time_func(1, 2, 3)
-long_time_func_2((5, 6, 7), 10)
-long_time_func_2((5, 6, 7), 10)
